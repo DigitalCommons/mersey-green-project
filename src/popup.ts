3082dde3d60d3e9bcf1e5e42cf2e0cf704239f39
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-05-31 19:14:55 +0100

    src/popup.ts - adapt from ObO public.
    Remove unwanted fields. Keep current pop-up layout.
    
    Type "any" parameters as "unknown".
    
    Make getWWW return an icon not text. Likewise getEmail.

--- src/popup.ts
+++ src/popup.ts
@@ -1,43 +1,14 @@
 import { DataServices } from "mykomap/src/map-app/app/model/data-services";
 import { Initiative } from "mykomap/src/map-app/app/model/initiative";
+import { Vocab } from "mykomap/src/map-app/app/model/vocabs";
 import { PhraseBook } from "mykomap/src/map-app/localisations";
-
-function getPhone(initiative: Initiative) {
-  // Not all orgs have a phone number
-  if (typeof initiative.tel === 'string') {
-    const phone = initiative.tel.replace(/^(\d)(\d{4})\s*(\d{6})/, "$1$2 $3");
-    return `<a class="fa fa-at" href="mailto:${phone}" target="_blank" ></a>`;
-  }
-  return "";
-}
-
-function getEmail(initiative: Initiative) {
-  // Not all orgs have an email
-  if (initiative.email)
-    return `<a class="fa fa-at" href="mailto:${initiative.email}" target="_blank" ></a>`;
-  return "";
-}
-
-function getFacebook(initiative: Initiative) {
-  // not all have a facebook
-  if (initiative.facebook)
-    return `<a class="fab fa-facebook" href="https://facebook.com/${initiative.facebook}" target="_blank" ></a>`;
-  return "";
-}
-
-function getTwitter(initiative: Initiative) {
-  // not all have twitter
-  if (initiative.twitter)
-    return `<a class="fab fa-twitter" href="https://twitter.com/${initiative.twitter}" target="_blank" ></a>`;
-  return '';
-}
 
 function getAddress(initiative: Initiative, getTerm: (prop: string) => string, labels: PhraseBook) {
   // We want to add the whole address into a single para
   // Not all orgs have an address
-  let address: string = "";
-  let street: string;
-  if (typeof initiative.street == 'string') {
+  let address = "";
+  let street;
+  if (typeof initiative.street === 'string') {
     let streetArray = initiative.street.split(";");
     for (let partial of streetArray) {
       if (partial === initiative.name) continue;
@@ -70,27 +41,82 @@ function getAddress(initiative: Initiative, getTerm: (prop: string) => string, l
 
 function getWWW(initiative: Initiative) {
   // Initiative's website. Note, not all have a website.
-  if (initiative.www)
-    return `<a class="fa fa-link" href="${initiative.www}" target="_blank" ></a>`
+  if (typeof initiative.www === 'string')
+    return `<a class="fa fa-link" href="${initiative.www}" target="_blank" ></a>`;
   return '';
 }
 
-export function getPopup(initiative: Initiative, dataservices: DataServices) {
-  function getTerm(propertyName: string) {
-    const propDef = dataservices.getPropertySchema(propertyName);
-    const propVal = initiative[propertyName];
-    if (propDef.type === 'vocab' && typeof propVal === 'string') {
-      const vocabUri = propDef.uri;
-      return dataservices.getVocabTerm(vocabUri, propVal);
-    }
-    throw new Error(`can't get term for non-vocab property ${propertyName}`);
+function getOrgStructure(initiative: Initiative, osVocab: Vocab) {
+
+  if (initiative.orgStructure instanceof Array && initiative.orgStructure.length > 0) {
+    const term = initiative.orgStructure.map((id: unknown) => osVocab.terms[String(id)]).join(", ");    
+    return `${osVocab.title}: ${term}`;
   }
 
-  const labels = dataservices.getFunctionalLabels();
-  
+  return '';
+}
+
+function getPrimaryActivity(initiative: Initiative, acVocab: Vocab) {
+  if (typeof initiative.primaryActivity === 'string' && initiative.primaryActivity != "") {
+    return `Main Activity: ${acVocab.terms[initiative.primaryActivity]}`;
+  }
+
+  return '';
+}
+
+function getSecondaryActivities(initiative: Initiative, acVocab: Vocab, labels: PhraseBook) {
+  const title = labels.secondaryActivities;
+
+  if (initiative.secondaryActivities instanceof Array && initiative.secondaryActivities.length > 0) {
+    const term = initiative.secondaryActivities.map((id: unknown) => acVocab.terms[String(id)]).join(", ");
+    return `${title}: ${term}`;
+  }
+
+  return '';
+}
+
+function getPhone(initiative: Initiative) {
+  // Not all orgs have a phone number
+  if (typeof initiative.tel === 'string') {
+    const phone = initiative.tel.replace(/^(\d)(\d{4})\s*(\d{6})/, "$1$2 $3");
+    return `<a class="fa fa-at" href="mailto:${phone}" target="_blank" ></a>`;
+  }
+  return "";
+}
+
+function getEmail(initiative: Initiative) {
+  // Not all orgs have an email
+  if (initiative.email)
+    return `<a class="fa fa-envelope" href="mailto:${initiative.email}" target="_blank" ></a>`;
+  return "";
+}
+
+function getFacebook(initiative: Initiative) {
+  // not all have a facebook
+  if (initiative.facebook)
+    return `<a class="fab fa-facebook" href="https://facebook.com/${initiative.facebook}" target="_blank" ></a>`;
+  return "";
+}
+
+function getTwitter(initiative: Initiative) {
+  // not all have twitter
+  if (initiative.twitter)
+    return `<a class="fab fa-twitter" href="https://twitter.com/${initiative.twitter}" target="_blank" ></a>`;
+  return '';
+}
+
+export function getPopup(initiative: Initiative, sse_initiatives: DataServices) {
+  const getTerm = sse_initiatives.getVocabs().getTerm;
+  const values = sse_initiatives.getLocalisedVocabs();
+  const labels = sse_initiatives.getFunctionalLabels();
+  const activtiesUri = "am:";
+  const orgStructUri = "os:";
   let popupHTML = `
     <div class="sea-initiative-details">
       <h2 class="sea-initiative-name">${initiative.name}</h2>
+      <h4 class="sea-initiative-economic-activity">${getPrimaryActivity(initiative, values[activtiesUri])}</h4>
+      <h4 class="sea-initiative-secondary-activity">${getSecondaryActivities(initiative, values[activtiesUri], labels)}</h5>
+      <h4 class="sea-initiative-org-structure">${getOrgStructure(initiative, values[orgStructUri])}</h4>
       <p>${initiative.desc || ''}</p>
     </div>
     
@@ -109,4 +135,3 @@ export function getPopup(initiative: Initiative, dataservices: DataServices) {
 
   return popupHTML;
 };
-
